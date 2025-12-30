0631b5438b529d8574f41032f6a80c9571898c2f
Dominik Tornow <dominik.tornow@gmail.com>
2025-12-30 11:13:50 +0000

    update intro

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,6 +136,10 @@ const config: Config = {
       rel: "stylesheet",
     },
     {
+      href: "https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&display=swap",
+      rel: "stylesheet",
+    },
+    {
       href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap",
       rel: "stylesheet",
     },
